81075a3b70cb0351e4e3b0dea685befe844fe96c
Arpan Pal <arpan522000@gmail.com>
2025-11-21 09:43:27 -0700

    fix formatting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="knock-knock-gmrt",
-    version="0.1.0",
+    version="1.0.0",
     author="Arpan Pal",
     author_email="",
     description="CLI tool to create GMRT observation reminders",
